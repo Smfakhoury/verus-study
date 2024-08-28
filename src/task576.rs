65494a662f341551bbb91c3617da802959d7a9b7
t-rijuljainMS <t-rijuljain@microsoft.com>
2024-08-28 06:57:19 -0700

    Update task576.rs

--- src/task576.rs
+++ src/task576.rs
@@ -5,6 +5,7 @@ fn main() {
     // Given a function in Rust that checks whether a list is 
     // a contiguous sublist of another or not, prove its correctness.
 
+    // Sample cases
     // input: [1,4,3,5] and [1,2], output: false
     // input: [1,2,1] and [1,2,1], output: true
     // input: [1,0,2,2] and [2,2,0], output: false
