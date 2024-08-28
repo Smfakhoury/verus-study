4a15edb9683a0c0c23f4ac02bbf6382ad52b5cce
t-rijuljainMS <t-rijuljain@microsoft.com>
2024-08-28 06:56:13 -0700

    Update task576.rs

--- src/task576.rs
+++ src/task576.rs
@@ -5,9 +5,9 @@ fn main() {
     // Given a function in Rust that checks whether a list is 
     // a contiguous sublist of another or not, prove its correctness.
 
-    assert!(!is_sub_array(&vec![1,4,3,5],&vec![1,2])); 
-    assert!(is_sub_array(&vec![1,2,1],&vec![1,2,1]));
-    assert!(!is_sub_array(&vec![1,0,2,2],&vec![2,2,0]));
+    // input: [1,4,3,5] and [1,2], output: false
+    // input: [1,2,1] and [1,2,1], output: true
+    // input: [1,0,2,2] and [2,2,0], output: false
 }
 
 verus! {
